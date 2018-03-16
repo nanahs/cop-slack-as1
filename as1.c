611784c6fd31774998e108b53f0c42fc49349850
Shanan Dalton <shanan_dalton@yahoo.com>
2018-03-16 03:00:15 -0400

    Filled in the structure of the assignment

--- as1.c
+++ as1.c
@@ -3,12 +3,15 @@
 
 #include <linux/module.h>
 #include <linux/kernal.h>
+#include <linux/string.h>
+#include <linux/init.h>
 
-static int s_open(struct inode*, struct file*);
-static int s_close(struct inode*, struct file*);
-static ssize_t s_read(struct file*, char*, size_t, loff_t*);
-static ssize_t s_write(struct file*, const char*, size_t, loff_t*);
+static int s_open(struct inode *, struct file *);
+static int s_close(struct inode *, struct file *);
+static ssize_t s_read(struct file *, char *, size_t, loff_t *);
+static ssize_t s_write(struct file *, const char*, size_t, loff_t *);
 
+//Setting the file operations structure to call our created functions
 static struct file_operations fops = 
 {
 	.open = s_open,
@@ -17,38 +20,87 @@ static struct file_operations fops =
 	.write = s_write
 };
 
+static int majorNum;
+static int readLoc = 0;
+
 int init_module(void)
 {
-	//todo
-	printk(KERN_INFO "Installing module.\n");
 
-	return 0;
+	//Register device with major device number
+	int regVal = register_chrdev(111, "as1", &fops);
+
+	if(regVal < 0){
+		//Device registration has failed
+		printk(KERN_ALERT "Device registration attempt has failed.\n");
+		return -1;
+	}
+	else
+	{
+		//Device registration was successful
+
+		majorNum = regVal;
+
+		printk(KERN_ALERT "Device has been registered. Major number: %d.\n", &majorNum);
+		return 0;
+	}
 }
 
 void cleanup_module(void)
 {
-	//to do
-	printk(KERN_INFO "Removing module.\n");
+	//Unregister the module
+	printk(KERN_ALERT "Unregistered device %d.\n", &majorNum);
+	unregister_chrdev(majorNum, "as1");
 }
 
-static int s_open(struct inode*, struct file*)
+static int s_open(struct inode *node, struct file *f)
 {
-
+	printk(KERN_ALERT "Device opened.\n");
+	return 0;
 }
 
-static int s_close(struct inode*, struct file*)
+static int s_close(struct inode *node, struct file *f)
 {
-
+	printk(KERN_ALERT "Device closed.\n");
+	return 0;
 }
 
-static ssize_t s_read(struct file*, char*, size_t, loff_t*)
+static ssize_t s_read(struct file *f, char *arr, size_t length, loff_t *off)
 {
+	printk(KERN_ALERT "Device read.\n");
 
+	int count = 0;
+
+	while(length && (msg[readLoc] != 0))
+	{
+		//Copies the byte to the user space
+		put_user(msg[readLoc], arr++);
+
+		count++;
+		length--;
+		readLoc++;
+	}
+
+	return count;
 }
 
-static ssize_t s_write(struct file*, const char*, size_t, loff_t*)
+static ssize_t s_write(struct file *f, const char *arr, size_t length, loff_t *off)
 {
+	printk(KERN_ALERT "Device write.\n");
 
+	int index = length-1;
+	int count = 0;
+	memset(msg, 0, 100);
+	readLoc = 0;
+	
+	while(length > 0)
+	{
+		//Writes the characters given in reverse
+		msg[count++] = arr[index--];
+		length--;
+	}
+
+	return count;
+	
 }
 
 
